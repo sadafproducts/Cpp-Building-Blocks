22d7a2b3e0e23badb3e92369e3fc42be592a62c7
Sadaf <sadaf.products@gmail.com>
2020-11-18 23:27:07 +0100

    B1 Input Output Project added

---
+++ B1 Input Output/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>	// Add this if you use cout or cin
+
+int main() {
+	std::cout << "Hello World!\n";	// Print text to console
+	std::cin.get();	// This keeps the window open
+	return 0;
+}
